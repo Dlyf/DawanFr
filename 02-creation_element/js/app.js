b89bf42ef0201bad9c0b6a431f69b6f3e32d45ad
ylesueur-dawan <ylesueur@dawan.fr>
2021-07-05 10:34:19 +0200

    Creation d'elements

--- 02-creation_element/js/app.js
+++ 02-creation_element/js/app.js
@@ -7,3 +7,21 @@ const element = React.createElement("h1", {}, "Hello World !");
 
 // Insertion de l'élément dans le DOM
 ReactDOM.render(element, document.getElementById('root'));
+
+
+// On ne peut utiliser ReactDOM.render() qu'une seule fois, sinon on écrase le contenu précédent
+// const element2 = React.createElement("h2", {}, "Toto !");
+// ReactDOM.render(element2, document.getElementById('root'));
+
+/**
+ * <ul>
+ *       <li>...</li>
+ * </ul>
+ */
+const listeElement = React.createElement("ul", {}, 
+    React.createElement('li', {}, 'Elephant'),
+    React.createElement('li', {className: 'li-item'}, 'Elan'),
+    React.createElement('li', {id: 'li-1'}, React.createElement("a", {href: '#'}, 'Epervier'))
+);
+
+ReactDOM.render(listeElement, document.getElementById('root'));
