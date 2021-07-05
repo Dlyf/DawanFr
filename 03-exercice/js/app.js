d005b9f819fe3e59ff63493432d62acbb43d45a8
ylesueur-dawan <ylesueur@dawan.fr>
2021-07-05 12:22:35 +0200

    Exercice

--- 03-exercice/js/app.js
+++ 03-exercice/js/app.js
@@ -5,10 +5,20 @@ ReactDOM.render(h2Element, document.getElementById('root'));
 const imgElement = React.createElement('img', {src: 'https://picsum.photos/200/300', alt: 'Aleatoire'});
 const pElement = React.createElement('p', null, 'Sunt ad proident deserunt voluptate aliquip minim qui voluptate minim est amet voluptate.');
 
+const headerElement = (
+    <header>
+        <h1>Lorem Ipsum</h1>
+        <i class="fas fa-shopping-cart"></i>
+        <p>0</p>
+    </header>
+);
 const divContainerElement = React.createElement('div', {}, 
     h2Element,
     imgElement,
-    pElement
+    pElement,
+    headerElement
 );
 
 ReactDOM.render(divContainerElement, document.getElementById('root'));
+
+
