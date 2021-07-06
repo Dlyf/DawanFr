28641ebf431a87078835dafee4978f97d5296d94
ylesueur-dawan <ylesueur@dawan.fr>
2021-07-06 15:07:52 +0200

    LEs evenements

--- 11-evenements/js/app.js
+++ 11-evenements/js/app.js
@@ -1,3 +1,24 @@
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { }
+    // }
+    state = {
+        prenoms: ['John', 'Maude', 'Ella', 'Rick']
+    }
 
+    sayHello() {
+        console.log("Bonjour !");
+    }
+
+    render() {
+        return (
+            <div>
+                {/* pas de parentheses lors de l'appel */}
+                <button onClick={this.sayHello}>Dire Bonjour</button>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App/>, document.getElementById('root'));
