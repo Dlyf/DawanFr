b1c7c901007144b16c71179627fc456b5429d6a0
ylesueur-dawan <ylesueur@dawan.fr>
2021-07-08 10:43:02 +0200

    Style & Classes CSS

--- 13-create-react-app/src/App.js
+++ 13-create-react-app/src/App.js
@@ -1,13 +1,16 @@
 // import Title from "./components/Title";
 // import CycleVie from "./components/01-cycle-vie/Cycle";
-import Compteur from "./components/02-remonter-etat/Compteur";
-
+// import Compteur from "./components/02-remonter-etat/Compteur";
+import Card from "./components/03-css/Card";
 function App() {
   return (
     <div className="App">
-        <Compteur/>
+        {/* <Compteur/> */}
         {/* <Title/> */}
         {/* <CycleVie/> */}
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed sunt debitis, mollitia ullam, et, nesciunt consectetur tenetur expedita ad vero earum molestiae repellat porro eius beatae laboriosam cupiditate unde enim!</p>
+        <a href="#">Salut !</a>
+        <Card/>
     </div>
   );
 }
---
+++ 13-create-react-app/src/components/03-css/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// Le CSS s'applique pour toute l'application
+import './Card.css';
+import styles from './Card.module.css';
+
+function Card(props) {
+    console.log(styles);
+    return (
+        <React.Fragment>
+            <div className={styles.card}>
+                <p>Card !</p>
+                <p className='test'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque voluptatum in exercitationem iure aspernatur deleniti hic quod consequuntur dolore dicta obcaecati dolores repudiandae cupiditate labore ut esse culpa, nam rerum?</p>
+            </div>
+        </React.Fragment>
+    );
+}
+export default Card;
