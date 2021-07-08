6946395055a8b4e366a61f6a33ea091d5da36e47
ylesueur-dawan <ylesueur@dawan.fr>
2021-07-08 16:39:06 +0200

    React Router

---
+++ 13-create-react-app/src/components/05-redux/ReduxCompteur.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ReduxCompteur(props) {
+    return (
+        <React.Fragment>
+            <h1>Compteur</h1>
+            <button>Incrementer</button>
+        </React.Fragment>
+    );
+}
+export default ReduxCompteur;
