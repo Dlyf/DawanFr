8f0b4cb1b9e4f6ffe4d513c396efa3e9100ba613
ylesueur-dawan <ylesueur@dawan.fr>
2021-07-09 09:43:12 +0200

    React Router

--- 13-create-react-app/src/components/05-redux/ReduxCompteur.jsx
+++ 13-create-react-app/src/components/05-redux/ReduxCompteur.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 function ReduxCompteur(props) {
     return (
         <React.Fragment>
-            <h1>Compteur: {null}</h1>
+            <h1>Compteur: {props.myCompteur}</h1>
             <button onClick={null}>
                 Incrementer
             </button>
@@ -11,4 +12,27 @@ function ReduxCompteur(props) {
     );
 }
 
-export default ReduxCompteur;
+/**
+ * Ajouter les variables de l'etat global (store)
+ * dans les props du composants.
+ * 2 parametres: 
+ *      - state: le state du store
+ *      - ownProps: les props du composants.
+ */
+function mapStateToProps(state, componentProps) {
+    console.log("State: ", state);
+    console.log("Les props du composants: ", componentProps);
+    const nouvellesProps = {
+        myCompteur: state.compteur
+    }
+    return nouvellesProps;
+}
+
+/**
+ * connect(): prend en parametre la fonction qui permet de fusionner le state et les props
+ * et renvoie une fonction qui permet de connecter le composant de notre choix.
+ */
+const composantAConnecter = connect(mapStateToProps);
+const connectedComponent = composantAConnecter(ReduxCompteur);
+// export default ReduxCompteur;
+export default connectedComponent;
