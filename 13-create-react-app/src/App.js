c04bad70368675931b7fa2a7422dba0abcb29d53
ylesueur-dawan <ylesueur@dawan.fr>
2021-07-09 09:29:22 +0200

    React Router

--- 13-create-react-app/src/App.js
+++ 13-create-react-app/src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navigation from './components/Navigation';
 import Routes from './router/Routes';
+import store from './store';
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-            <Navigation/>
-            <Routes/>
-        </BrowserRouter>
+        {/* Le store est maintenant accessible partout dans l'application */}
+        <Provider store={store}>
+          <BrowserRouter>
+              <Navigation/>
+              <Routes/>
+          </BrowserRouter>
+        </Provider>
     </div>
   );
 }
